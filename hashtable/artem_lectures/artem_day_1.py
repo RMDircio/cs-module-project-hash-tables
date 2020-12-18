e67e2991b1cedf4fec6f392fe3b54f870d719c45
RMDircio <reginadircio@gmail.com>
2020-12-17 22:06:41 -0600

    Updates from CSPT11 Lecture

--- hashtable/artem_lectures/artem_day_1.py
+++ hashtable/artem_lectures/artem_day_1.py
@@ -1,5 +1,7 @@
 '''         ARTEM'S CSPT8 LECTURE 
 https://www.youtube.com/watch?v=NwsLkmf6r0c&feature=youtu.be
+            ARTEM'S CSPT11 LECTURE 
+https://www.youtube.com/watch?v=StGch8xUmNQ&feature=youtu.be
 '''
 ####            --------------------------------------------------------------
 ####                                HASH FUNCTIONS DAY 1 
@@ -48,10 +50,10 @@ def my_hash1(c):
     # convert string into UTF --> use encode()
     string_utf = c.encode() # returns an array
 
-    for char in string_utf:
-        print (char)
+    for byte_char in string_utf:
+        print (byte_char)
 
-my_hash1('Boop the Snoot')
+# my_hash1('Boop the Snoot')
 
 ''' OUTPUT 
             66
@@ -71,51 +73,111 @@ my_hash1('Boop the Snoot')
 '''
 
 
-# # 3rd example - to get 1 number as the sum of encoded string - also returning 32 bit number or less
-# def my_hash2(c, length_limit):
-#     string_utf = c.encode()
+# 3rd example - to get 1 number as the sum of encoded string - also returning 32 bit number or less
+def my_hash2(c, length_limit): # O(n) where n is length of string
+    string_utf = c.encode()
 
-#     total = 0
-#     for char in string_utf:
-#         total += char
-#         # make hash function 32 bit
-#         total &= 0xffffffff # bitwise operation eight 'f' here (hexadecimal system) clamps big numbers down to 32 bits (16 f for 64 bits)
+    total = 0
+    for char in string_utf:
+        total += char
+        # make hash function 32 bit
+        total &= 0xffffffff # bitwise operation eight 'f' here (hexadecimal system) clamps big numbers down to 32 bits (16 f for 64 bits)
     
-#     # divide the total by the length limit but only return the remainder
-#     return total % length_limit # breaks down the large number into a range from 0-8 (the limit) 
+    # divide the total by the length limit but only return the remainder
+    return total % length_limit # breaks down the large number into a range from 0-8 (the limit) 
 
-# # print(f'The sum of "Boop the Snoot" in Unicode:', my_hash2('Boop the Snoot'))
+# not sure why this is broken
+# print(f'The sum of "Boop the Snoot" in Unicode:', my_hash2('Boop the Snoot'))
+
+### CSPT11 3rd example
+def hash_fn(s): # O(n) where n is length of string
+    print(s)
+    encoded_string = s.encode() # O(1)
+    
+    result = 0
+    for byte_char in encoded_string:
+        result += byte_char
+    
+    return result
+
+### CSPT11 4th example
+
+# getting an index
+hash_array = [None] * 8
+# map the results of function 'hash_fn' above to an index in some array
+# use modulo to bind a number from 'hash_fn' to 0 --> length of the array
+
+# set the hash value 
+hash_value = hash_fn('banana')
+
+# get the index with modulo
+index = hash_value % len(hash_array)
+print(index)
+
+# set the index to a value
+hash_array[index] = 'is yellow'
+print(hash_array)
+
+
+
+
+
+# print(hash_fn('apple'))
+''' OUTPUT 
+        530
+'''
 
 
 # ####            --------------------------------------------------------------            ####
 # ####                                CREATE HASH TABLE                                     ####
 # ####            --------------------------------------------------------------            ####
 
+'''
+Hash Function + An Array == HashTable
 
-# # hash table is an array :)
-# hash_table = [None] * 8 # eight elements of 'None'
+To insert a key and value to this HashTable
+    - hash the key to convert it to a number
+    - take that number and MOD it by the size of the HashTable
+    - insert the VALUE into the index given by the MOD operation
+'''
 
-# # ADD items to hash table - use the my_hash2 function
-# # store the sum of the word to an element in the hash table
-# index = my_hash2('Boop', len(hash_table)) # 400 (sum of Boop) % 8 (length of table) = 0
-# # print(index)
+# hash table is an array :)
+hash_table = [None] * 8 # eight elements of 'None'
 
-# hash_table[index] = 'Value for Boop'
-# # print(hash_table) # OUTPUT: ['Value for Boop', None, None, None, None, None, None, None]
+# ADD items to hash table - use the my_hash2 function
+# store the sum of the word to an element in the hash table
+index = my_hash2('Boop', len(hash_table)) # 400 (sum of Boop) % 8 (length of table) = 0
+# print(index)
+''' OUTPUT 
+            0
+'''
+hash_table[index] = 'Value for Boop'
+# print(hash_table) 
+''' OUTPUT 
+            ['Value for Boop', None, None, None, None, None, None, None]
+'''
+
+# 2nd example - bound word to a number between 0-8 (length of the array)
+index = my_hash2('the', len(hash_table)) # 400 (sum of the) % 8 (length of table) = 0
+# print(index)
+''' OUTPUT 
+            1
+'''
+hash_table[index] = 'Value for the'
+# print(hash_table) 
+''' OUTPUT 
+            ['Value for Boop', 'Value for the', None, None, None, None, None, None]
+'''
+
+# retrieve value/items from hash table
+index = my_hash2('Boop', len(hash_table)) # storing
+print(hash_table[index]) # retriving
+''' OUTPUT 
+            Value for Boop
+'''
 
 
-# # 2nd example
-# index = my_hash2('the', len(hash_table)) # 400 (sum of th) % 8 (length of table) = 0
-# # print(index) # OUTPUT: 1
-# hash_table[index] = 'Value for the'
-# # print(hash_table) # OUTPUT: ['Value for Boop', 'Value for the', None, None, None, None, None, None]
-
-
-# # retrieve value/items from hash table
-# index = my_hash2('Boop', len(hash_table))
-# # print(hash_table[index])
-
-# # RUNTIME of adding and retrieveing - O(n) since hash function is O(n) 
+# RUNTIME of adding and retrieveing - O(n) since hash function is O(n) 
 
 
 # ####            --------------------------------------------------------------            ####
